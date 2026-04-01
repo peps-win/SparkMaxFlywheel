f77076da9a0a90365f12cbaa072f7aa04be0c87f
Alex Skillman <askillman2010@gmail.com>
2026-03-31 23:41:14 -0500

    New changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,8 +2,13 @@ package frc.robot;
 
 public final class Constants {
     public static class FlywheelShooterConstants {
+       
         public static final int MOTORID= 1;
         public static final int FLYWHEEL_CURRENT_LIMIT = 60; // Amps
         public static final double FLYWHEEL_VOLTAGE = 11; // Volts
+
+        public static final double kP = 0;
+        public static final double kI = 0;
+        public static final double kD = 0;
     }
 }
